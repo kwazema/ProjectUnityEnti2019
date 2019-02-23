316d828f7342a65611fdf2b0afaba5a8060b93c2
Alex <alexmrtc@gmail.com>
2019-02-23 18:30:28 +0100

    Animations in Rogue Character

---
+++ ABlastFromThePast/Assets/Animations/Animator_Rogue.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animator_Rogue : MonoBehaviour
+{
+    public Animator animator;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            animator.SetBool("isAttacking", true);
+        }
+        else if (Input.GetButtonUp("Fire1"))
+        {
+            animator.SetBool("isAttacking", false);
+        }
+
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            animator.SetFloat("speed", 0.2f);
+        } else if (Input.GetButtonUp("Horizontal"))
+        {
+            animator.SetFloat("speed", 0);
+        }
+    }
+}
+
