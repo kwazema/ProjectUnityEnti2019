f4fb69fec7bc15fd56ecb226b1699037bc12bdbc
Alex <alexmrtc@gmail.com>
2019-02-26 08:48:22 +0100

    Main menu

---
+++ ABlastFromThePast/Assets/Scenes/Almorax/TestScenes/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void Play() 
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Practice()
+    {
+        Debug.Log("Practica moffo!");
+        //SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Online()
+    {
+        Debug.Log("About to get rekt");
+        //SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
