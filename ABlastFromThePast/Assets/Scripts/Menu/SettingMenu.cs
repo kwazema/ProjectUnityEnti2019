183841f16d96cdbb775678f18e6a7e0950147494
Alex <alexmrtc@gmail.com>
2019-03-15 14:31:00 +0100

    Language

--- ABlastFromThePast/Assets/Scripts/Menu/SettingMenu.cs
+++ ABlastFromThePast/Assets/Scripts/Menu/SettingMenu.cs
@@ -5,10 +5,58 @@ using UnityEngine.Audio;
 
 public class SettingMenu : MonoBehaviour {
 
+    public GameObject gameObject;
+
+    enum language
+    {
+        catalan,
+        spanish, 
+        english
+    };
+
+    language idiom;
+
     public AudioMixer audioMixer;
   
     public void Volume ( float volume)
     {
         audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetLanguageCatalan()
+    {        
+        idiom = language.catalan;
+    }
+
+    public void SetLanguageSpanish()
+    {
+        idiom = language.spanish;
+    }
+
+    public void SetLanguageEnglish()
+    {
+        idiom = language.english;
+    }
+
+    public void ShowContent()
+    {
+        GameObject.Find("OptionsMenuCat").SetActive(false);
+        GameObject.Find("OptionsMenuEsp").SetActive(false);
+        GameObject.Find("OptionsMenuEng").SetActive(false);
+        switch (idiom)
+        {
+            case language.catalan:
+                GameObject.Find("OptionsMenuCat").SetActive(true);
+                Debug.Log("catalan");
+                break;
+            case language.spanish:
+                GameObject.Find("OptionsMenuEsp").SetActive(true);
+                Debug.Log("spanish");
+                break;     
+            case language.english:
+                GameObject.Find("OptionsMenuEng").SetActive(true);
+                Debug.Log("english");
+                break;
+        }
     }
 }
