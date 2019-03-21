0c980a10f39309f65e4b8caf1031dadba16c8d1f
Andreu <andreu.kwazema@gmail.com>
2019-03-21 09:32:36 +0100

    Refactorization stats script

---
+++ ABlastFromThePast/Assets/Scripts/Characters/SwipperStats.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipperStats : Stats {
+
+    // Use this for initialization
+    protected override void Start () {
+        base.Start();
+
+        health = 100;
+        shield = 20;
+        damageBasicAttack = 3;
+        damageSkill = 10;
+        damageUltimate = 50;
+        fireRate = 0.2f;
+        recoveryShieldTime = 2;
+
+        StartCoroutine(ShieldRecovery());
+
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+    }
+}
