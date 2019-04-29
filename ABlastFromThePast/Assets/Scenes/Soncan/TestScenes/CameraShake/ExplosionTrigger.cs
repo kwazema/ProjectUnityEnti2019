f6c991f81b38b84bdc3f5110d077ed30cd0539cb
Kwazema <andreu.kwazema@gmail.com>
2019-04-29 22:14:19 +0200

    Added new UI Battle & First test slider

--- ABlastFromThePast/Assets/Scenes/Soncan/TestScenes/CameraShake/ExplosionTrigger.cs
+++ ABlastFromThePast/Assets/Scenes/Soncan/TestScenes/CameraShake/ExplosionTrigger.cs
@@ -1,32 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class ExplosionTrigger : MonoBehaviour {
+//public class ExplosionTrigger : MonoBehaviour {
 
-    public GameObject explosion;
-    public CameraShake cameraShake;
-    public Transform explosionPosition;
+//    public gameobject explosion;
+//    public transform explosionposition;
+//    public CameraShake cameraShake;
+//    public float
 
-    // Use this for initialization
-    void Start () {
+//    // Use this for initialization
+// //   void Start () {
 		
-	}
+//	//}
 	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            Debug.Log("Explosion");
-            Instantiate(explosion, explosionPosition.position, Quaternion.identity);
-            StartCoroutine(cameraShake.Shake(.95f, .94f));
-        }
-    }
+//	// Update is called once per frame
+//	void Update () {
+//        if (Input.GetKeyDown(KeyCode.A))
+//        {
+//            Debug.Log("Explosion");
+//            Instantiate(explosion, explosionPosition.position, Quaternion.identity);
+//            StartCoroutine(cameraShake.Shake(.95f, .94f));
+//        }
+//    }
 
-    private void OnMouseEnter()
-    {
-        Debug.Log("Enter Mouse");
-    }
+//    private void OnMouseEnter()
+//    {
+//        Debug.Log("Enter Mouse");
+//    }
 
 
-}
+//}
---
+++ ABlastFromThePast/Assets/Scripts/PlayeUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayeUI : MonoBehaviour {
+
+    public Image[] icon;
+    public Image[] sliderHealth;
+    public Image[] sliderSkill;
+    public Image[] sliderUltimate;
+    public Image[] sliderShield;
+    public string[] namePlayer;
+
+    private GameManager gameManager;
+
+    private void Awake()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    private void Start()
+    {
+		
+	}
+
+    private void Update()
+    {
+        float test = gameManager.playerStats[0].GetHealth() * 1f / gameManager.playerStats[0].GetHealthMax();
+        Debug.Log("Vida: " + test);
+        Debug.Log("Vida Total: " + gameManager.playerStats[0].GetHealth());
+        Debug.Log("Vida Maxima: " + gameManager.playerStats[0].GetHealthMax());
+        sliderHealth[0].fillAmount = test;
+	}
+}
