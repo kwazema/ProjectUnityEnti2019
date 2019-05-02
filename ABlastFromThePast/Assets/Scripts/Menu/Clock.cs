1c867ec4c3777b0d3d7222c522983f37ccd956fe
Alex <alexmrtc@gmail.com>
2019-05-02 14:14:52 +0200

    Add Clock and redesign menus

---
+++ ABlastFromThePast/Assets/Scripts/Menu/Clock.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour {
+    public GameObject clockManecilla1;
+    public GameObject clockManecilla2;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        clockManecilla1.transform.Rotate(Vector3.forward * -1);
+        clockManecilla2.transform.Rotate(Vector3.forward * -0.075f);
+    }
+}
