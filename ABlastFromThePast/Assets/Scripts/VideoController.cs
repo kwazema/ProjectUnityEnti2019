8db134c18275ac06907a633a4689c95a7bd4c1b1
Alex <alexmrtc@gmail.com>
2019-05-11 14:05:16 +0200

    Added Script VideoController to Scene

---
+++ ABlastFromThePast/Assets/Scripts/VideoController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class VideoData
+{
+    public VideoClip general;
+
+    public VideoClip basic;
+    public VideoClip skill;
+    public VideoClip ultimate;
+
+    public VideoClip shield;
+
+
+    public void SetGeneral(VideoPlayer vp)
+    {
+        vp.clip = skill;
+    }
+
+    public void SetBasic(VideoPlayer vp)
+    {
+        vp.clip = ultimate;
+    }
+
+}
+
+public class VideoController : MonoBehaviour {
+    public RawImage rawImage;
+    public VideoPlayer videoPlayer;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(PlayVideo());
+	}
+
+    IEnumerator PlayVideo()
+    {
+        videoPlayer.Prepare();
+        WaitForSeconds waitForSeconds = new WaitForSeconds(0.5f);
+        while (!videoPlayer.isPrepared)
+        {
+            yield return waitForSeconds;
+            break;
+        }
+        rawImage.texture = videoPlayer.texture;
+        videoPlayer.Play();
+    }	
+}
