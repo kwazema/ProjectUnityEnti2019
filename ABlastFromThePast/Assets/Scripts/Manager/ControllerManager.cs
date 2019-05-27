392d1061b3ddad562a747618d7520d391e334573
Kwazema <andreu.kwazema@gmail.com>
2019-05-27 18:55:34 +0200

    Config vibrationController damage & start round

---
+++ ABlastFromThePast/Assets/Scripts/Manager/ControllerManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XInputDotNetPure;
+
+[System.Serializable]
+public class ControllerManager {
+
+    static public IEnumerator ControllerVibration(int playerIndex, float leftMotor, float rightMotor, float time, float timeToStart = 0)
+    {
+
+        yield return new WaitForSeconds(timeToStart);
+
+        GamePad.SetVibration((PlayerIndex)playerIndex, leftMotor, rightMotor);
+
+        yield return new WaitForSeconds(time);
+
+        GamePad.SetVibration((PlayerIndex)playerIndex, 0, 0);
+    }
+}
